6e71681e0b0f786ed30dcebe636b732ddc9a6de5
Mikko Esko <mikko.esko@tuni.fi>
2019-12-01 07:58:39 +0200

    updated template

--- template.jl
+++ template.jl
@@ -1,13 +1,16 @@
 using Printf
 
 function main()
-    #lines = readlines("input.txt") 
+    lines = readlines("input.txt")
+    #numbers = map(x -> parse(Int, x), readlines("input.txt"))
     #chars = read("input.txt", Str)
     #word_lists = map(x -> split(x, " "), readlines("input.txt"))
     #dict = Dict(tuple(map(x -> split(x, ": "), readlines("input.txt"))))
+    result = 0
     for line in lines
         println(line)
     end
+    @printf "Result is %d\n" result
 end
 
-main()
+@time main()
